aabcfa1c4592bb35a4bfaefd587e4d70a0ff0d2f
vishnuacappella <acappella.studiov@gmail.com>
2022-07-04 05:44:31 +0530

    dart file created

---
+++ bin/const_vs_final.dart
@@ -0,0 +1,5 @@
+import 'package:const_vs_final/const_vs_final.dart' as const_vs_final;
+
+void main(List<String> arguments) {
+  print('Hello world: ${const_vs_final.calculate()}!');
+}
---
+++ test/const_vs_final_test.dart
@@ -0,0 +1,8 @@
+import 'package:const_vs_final/const_vs_final.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
